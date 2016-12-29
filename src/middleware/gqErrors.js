953df406ab1de731e7679a78d222c584cd6d3080
Lei Yang <leiyangyou@gmail.com>
2016-12-30 06:25:25 +0800

    dealing with brunch envify problem

--- src/middleware/gqErrors.js
+++ src/middleware/gqErrors.js
@@ -71,7 +71,7 @@ function noticeAbsentStack() {
       schema: myGraphQLSchema,
       formatError: (error) => ({
         message: error.message,
-        stack: process.env.NODE_ENV === 'development' ? error.stack.split('\\n') : null,
+        stack: process.['env'].NODE_ENV === 'development' ? error.stack.split('\\n') : null,
       })
     });
 
